a99c5a1ae6b225d483641d05e214ad2b95ad4888
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-08-17 13:06:40 -0500

    [chore]: added date to today's studies notepad

--- components/TodayNotepad.tsx
+++ components/TodayNotepad.tsx
@@ -1,3 +1,4 @@
+import { TODAY_JP } from '@/lib/globals';
 import { getTodaysStudies } from '@/lib/notion';
 import { getIconForCategory } from '@/lib/utils';
 import Window from './Window';
@@ -12,14 +13,17 @@ const TodayNotepad = async () => {
   return (
     <Window English="Today's Studies" Japanese='今日の勉強' width='w-96' height='h-72'>
       <div className='h-64 bg-slate-100 border-2 border-dark-gray rounded-sm p-4 overflow-y-scroll'>
-        {activities.map((activity) => (
-          <div key={activity.id} className='flex flex-row text-sm pb-4 items-center'>
-            <span className='pr-2 shrink-0'>{getIconForCategory(activity.category)}</span>
-            <span>
-              {activity.title} {activity.media && `～ ${activity.media}`}
-            </span>
-          </div>
-        ))}
+        <div className='border-b-2 border-black'>{TODAY_JP}</div>
+        <div className='pt-2'>
+          {activities.map((activity) => (
+            <div key={activity.id} className='flex flex-row text-sm pb-4 items-center'>
+              <span className='pr-2 shrink-0'>{getIconForCategory(activity.category)}</span>
+              <span>
+                {activity.title} {activity.media && `～ ${activity.media}`}
+              </span>
+            </div>
+          ))}
+        </div>
         <div>• |</div>
       </div>
     </Window>
